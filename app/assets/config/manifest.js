0b879d9daee0a121c2cc26f38098f4621e1e8d65
Jacek Podkanski <ruby.object@googlemail.com>
2023-05-13 16:31:42 +0100

    gitignore

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../images
 //= link_tree ../builds
 
-//= link plugins/home.js
+//= link_tree ../javascript/plugins
--- vendor/Elm/Home/compile_home.sh
+++ vendor/Elm/Home/compile_home.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 elm make ./src/Main.elm --output home.js
-mv ./home.js ../../../app/javascript/home.js
+mv ./home.js ../../../app/javascript/plugins/home.js
