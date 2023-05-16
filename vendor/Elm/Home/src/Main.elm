ed5d3e02f6be0e8dfeffaed5a4ec51db9b39b400
Jacek Podkanski <ruby.object@googlemail.com>
2023-05-16 18:52:40 +0100

    started decoding

--- vendor/Elm/Home/src/Main.elm
+++ vendor/Elm/Home/src/Main.elm
@@ -5,6 +5,7 @@ import Html exposing (Html, button, div, hr, span, text)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 import Http
+import Json.Decode exposing (Decoder, field, int, map3, string)
 import List
 
 
@@ -17,12 +18,16 @@ main =
         }
 
 
-init : Flags -> ( Model, Cmd msg )
+init : Flags -> ( Model, Cmd Msg )
 init flags =
     ( { flags = flags
       , cnt = 0
+      , result = ""
       }
-    , Cmd.none
+    , Http.get
+        { url = "http://localhost:3000/texts.json"
+        , expect = Http.expectString GotTextString
+        }
     )
 
 
@@ -33,6 +38,7 @@ type alias Flags =
 type alias Model =
     { flags : Flags
     , cnt : Int
+    , result : String
     }
 
 
@@ -46,15 +52,54 @@ type alias Text =
 type Msg
     = Increment
     | Decrement
+    | GotTextString (Result Http.Error String)
+    | GotText (Result Http.Error Text)
 
 
 update msg model =
     case msg of
         Increment ->
-            ( { model | cnt = model.cnt + 1 }, Cmd.none )
+            ( { model
+                | cnt = model.cnt + 1
+                , result = ""
+              }
+            , Cmd.none
+            )
 
         Decrement ->
-            ( { model | cnt = model.cnt - 1 }, Cmd.none )
+            ( { model | cnt = model.cnt - 1 }, getTexts )
+
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( { model | result = Debug.toString fullText }, Cmd.none )
+
+                Err err ->
+                    ( { model | result = Debug.toString err }, Cmd.none )
+
+        GotTextString result ->
+            case result of
+                Ok fullText ->
+                    ( { model | result = fullText }, Cmd.none )
+
+                Err err ->
+                    ( { model | result = Debug.toString err }, Cmd.none )
+
+
+host =
+    "http://localhost:3000"
+
+
+getTexts =
+    Http.get { url = host ++ "/texts.json", expect = Http.expectJson GotText gotTextDecoder }
+
+
+gotTextDecoder : Decoder Text
+gotTextDecoder =
+    map3 Text
+        (field "id" string)
+        (field "link" string)
+        (field "body" string)
 
 
 view model =
