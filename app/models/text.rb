cc300c856f7d70cf2a73fffb846391ada20fdba0
Jacek Podkanski <ruby.object@googlemail.com>
2023-05-20 00:37:08 +0100

    cleanup

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,21 +1,18 @@
 class Text < ApplicationRecord
-
   def words
-    body.split(',').join(' ')#.split('.').join(' ').split(' ').collect(&:downcase)
-      .gsub(/(\.|\,)/, ' ')
-      .split
-      .collect(&:downcase)
+    body.split(',').join(' ') # .split('.').join(' ').split(' ').collect(&:downcase)
+        .gsub(/(\.|\,)/, ' ')
+        .split
+        .collect(&:downcase)
   end
 
   def counts
     hash = {}
     words.sort.reverse.each do |word|
-      if hash[word].nil?
-        hash[word] = 0
-      end
+      hash[word] = 0 if hash[word].nil?
       hash[word] += 1
     end
 
-    hash.to_a.sort{ |a, b| a.second <=> b.second }.reverse
+    hash.to_a.sort { |a, b| a.second <=> b.second }.reverse
   end
 end
